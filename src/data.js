133720e529aa95638c79a756821afc6afcb25922
Tunahanclr16 <tunahancelerr16@gmail.com>
2024-04-16 20:28:46 +0300

    new project add

--- src/data.js
+++ src/data.js
@@ -6,12 +6,18 @@ import disney from "./assets/data/disney.png"
 import manager from "./assets/data/manager.png"
 import beaches from "./assets/data/beaches.png"
 import slider from "./assets/data/slider.png"
-
-
+import course from "./assets/data/course.png"
 
  const data=[
     {
         id:1,
+        title:"Course Project",
+        image:course,
+        github:"https://github.com/Tunahanclr16/Course-Project",
+        live:"https://course-project-ddxk.vercel.app/",
+    },
+    {
+        id:3,
         title:"Digitalagency Landing Page",
         image:Digital,
         github:"https://github.com/Tunahanclr16/DigitalAgeny-page",
@@ -25,42 +31,42 @@ import slider from "./assets/data/slider.png"
         live:"https://e-commerce-y17u.vercel.app/",
     },
     {
-        id:3,
+        id:4,
         title:"Product Manager ", 
         image:manager,
         github:"https://github.com/Tunahanclr16/add-product-",
         live:"https://add-product-phi.vercel.app/"
     },
     {
-        id:4,
+        id:5,
         title:"Nextjs Landing Page", 
         image:terappin,
         github:"https://github.com/Tunahanclr16/nextjs-task?tab=readme-ov-file",
-        live:"https://nextjs-task-fj2l.vercel.app/"
-    },
-    {
-        id:5,
-        title:"Disney ", 
-        image:disney,
-        github:"https://github.com/Tunahanclr16/disney-landing-page-clone",
-        live:"https://disney-clone-eight-orcin.vercel.app"
+        live:"https://nextjs-task-omega.vercel.app/"
     },
     {
         id:6,
+        title:"Disney landing page", 
+        image:disney,
+        github:"https://github.com/Tunahanclr16/disney-landing-page-clone",
+        live:"https://disney-clone-red-pi.vercel.app/"
+    },
+    {
+        id:7,
         title:"Beach Website", 
         image:beaches,
         github:"https://github.com/Tunahanclr16/Beaches-website",
         live:"https://beaches-website-one.vercel.app"
     },
     {
-        id:7,
+        id:8,
         title:"Fylo landing", 
         image:fylo,
         github:"https://github.com/Tunahanclr16/fylo-landing-page",
         live:"https://fylo-landing-page-six-theta.vercel.app/"
     },
     {
-        id:8,
+        id:9,
         title:"Slider control panel", 
         image:slider,
         github:"https://github.com/Tunahanclr16/admin-slider-component",
